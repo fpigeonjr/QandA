9739c4d02f69cc27020f60dfc254bda3bd51a4ff
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-01-19 03:07:16 -0600

    styles homepage, header, icons, and adds button

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { DEFAULT_MAX_VERSION } from 'tls';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { PrimaryButton } from './Styles';
 
 export const HomePage = () => (
-  <div>
-    <div>
+  <div
+    css={css`
+      margin: 50px auto 20px auto;
+      padding: 30px 20px;
+      max-width: 600px;
+    `}
+  >
+    <div
+      css={css`
+        font-size: 15px;
+        font-weight: bold;
+        margin: 10px 0 5px;
+        margin-align: center;
+        text-transform: uppercase;
+      `}
+    >
       <h2>Unanswered Questions</h2>
-      <button>Ask a question</button>
+      <PrimaryButton>Ask a question</PrimaryButton>
     </div>
   </div>
 );
--- frontend/src/Icons.tsx
+++ frontend/src/Icons.tsx
@@ -1,4 +1,16 @@
 import React from 'react';
 import userSvg from './user.svg';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
 
-export const UserIcon = () => <img src={userSvg} alt="user" width="12px" />;
+export const UserIcon = () => (
+  <img
+    src={userSvg}
+    alt="user"
+    width="12px"
+    css={css`
+      width: 12px;
+      opacity: 0.6;
+    `}
+  />
+);
