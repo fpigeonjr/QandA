ea1b0a92238339e6a3318c2431596053253c5287
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-01-19 02:09:14 -0600

    adds a homepage comp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Header } from './Header';
+import { HomePage } from './HomePage';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+      <HomePage />
     </div>
   );
 };
---
+++ frontend/src/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { DEFAULT_MAX_VERSION } from 'tls';
+
+export const HomePage = () => (
+  <div>
+    <div>
+      <h2>Unanswered Questions</h2>
+      <button>Ask a question</button>
+    </div>
+  </div>
+);
