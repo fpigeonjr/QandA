253fa5b902d051cbbb2012d873cb6192a09566c1
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-01-19 23:20:06 -0600

    adds optional and default props

--- frontend/src/Question.tsx
+++ frontend/src/Question.tsx
@@ -2,13 +2,14 @@ import React, { FC } from 'react';
 /**@jsx jsx */
 import { css, jsx } from '@emotion/core';
 import { QuestionData } from './QuestionsData';
-import { gray3 } from './Styles';
+import { gray2, gray3 } from './Styles';
 
 interface Props {
   data: QuestionData;
+  showContent?: boolean;
 }
 
-export const Question: FC<Props> = ({ data }) => (
+export const Question: FC<Props> = ({ data, showContent = true }) => (
   <div
     css={css`
       padding: 10px 0;
@@ -22,6 +23,19 @@ export const Question: FC<Props> = ({ data }) => (
     >
       {data.title}
     </div>
+    {showContent && (
+      <div
+        css={css`
+          padding-bottom: 10px;
+          font-size: 15px;
+          color: ${gray2};
+        `}
+      >
+        {data.content.length > 50
+          ? `${data.content.substring(0, 50)}...`
+          : data.content}
+      </div>
+    )}
     <div
       css={css`
         font-size: 12px;
