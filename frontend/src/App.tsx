49e6ed7c0fc1c4cc5ca72379e58bc4a25d10f900
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-01-27 00:13:44 -0600

    adds new vanilla pages and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { AskPage } from './AskPage';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
+import { NotFoundPage } from './NotFoundPage';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 import { fontFamily, fontSize, gray2 } from './Styles';
 
 const App: React.FC = () => {
   return (
-    <div
-      css={css`
-        font-family: ${fontFamily};
-        font-size: ${fontSize};
-        color: ${gray2};
-      `}
-    >
-      <Header />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        <Switch>
+          <Redirect from="/home" to="/" />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/search" component={SearchPage} />
+          <Route path="/ask" component={AskPage} />
+          <Route path="/signin" component={SignInPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 };
 
---
+++ frontend/src/NotFoundPage.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react';
+import { Page } from './Page';
+export const NotFoundPage = () => <Page title="Page Not Found" />;
